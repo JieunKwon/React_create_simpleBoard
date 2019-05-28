89ac7730dea64c30bd0ee4d3fd1be1b0225cc79b
Julia Jieun <39966599+JieunKwon@users.noreply.github.com>
2019-05-28 10:25:33 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// Julia Jieun Kwon
+// @ May 2019
+
 class App extends Component {
 
     constructor(props) {
       super(props);
       this.child = React.createRef();
     }
+
     state = {
 
         maxNo: 3,   // record for next no of boardno
@@ -41,7 +45,7 @@ class App extends Component {
 
     }
 
-    // add new data
+    // To add new data
     // handleSaveData = (data) => {
     //   this.setState({
     //     maxNo: this.state.maxNo+1,
@@ -49,7 +53,37 @@ class App extends Component {
     //   });
     // }
 
-    // remove the selected data using filter
+    // To edit data, updated handleSaveData
+    handleSaveData = (data) => {
+
+      let boards = this.state.boards;
+
+      // INSERT - if no brdno, the data is new one
+      if (data.brdno == null || data.brdno === '' || data.brdno === undefined) {
+
+          this.setState ({
+
+              maxNo: this.state.maxNo+1,
+
+              boards: boards.concat({
+
+                brdno: this.state.maxNo, brddate: new Date(), ...data
+
+              })
+         });
+
+      // UPDATE - if brdno exists, the data of brdno will be updated
+      } else {
+
+        this.setState ({
+
+            boards: boards.map(row => data.brdno === row.brdno ? {...data}:row)
+
+            })
+        }
+    }
+
+    // Remove the selected data using filter
     handleRemove = (brdno) => {
       this.setState({
         boards: this.state.boards.filter(row => row.brdno !== brdno)
@@ -62,6 +96,12 @@ class App extends Component {
 
     }
 
+    handleSelectRow = (row) => {
+
+      this.child.current.handleSelectRow(row);
+
+    }
+
     render() {
 
         const { boards } = this.state;
@@ -70,7 +110,8 @@ class App extends Component {
 
            <div>
 
-              <BoardForm selectedBoard = {selectedBoard} onSaveData = {this.handleSaveData} />
+
+              <BoardForm  onSaveData = {this.handleSaveData} ref={this.child}/>
 
                    {
 
@@ -101,6 +142,10 @@ class BoardForm extends React.Component{
     brdtitle:''
   }
 
+  handleSelectRow = (row) => {
+    this.setState(row);
+  }
+
   handleSelectedRow = (row) => {
       this.setState(row);
   }
@@ -120,7 +165,7 @@ class BoardForm extends React.Component{
         this.setState({
           brdno:'',
           brdwriter:'',
-          brdtitle:'
+          brdtitle:''
         });
 
     }
@@ -146,12 +191,17 @@ class BoardForm extends React.Component{
     }
 }
 
-// to return one borad item by a table 
+// to return one borad item into table tag
 class BoardItem extends React.Component {
 
     handleRemove = () => {
       const {row, onRemove} = this.props;
       onRemove(row.brdno);
+    }
+
+    handleSelectRow = () => {
+      const {row, onSelectRow} = this.props;
+      onSelectRow(row);
     }
 
     render() {
@@ -169,7 +219,7 @@ class BoardItem extends React.Component {
             </tr>
             <tr>
                 <td class="BoardItem-title" colspan="2">
-                  {this.props.row.brdtitle}
+                  <a onClick={this.handleSelectRow}>{this.props.row.brdtitle}</a>
                 </td>
             </tr>
          </table>
